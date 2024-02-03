d9142b906d7300107e89b441fec4722ab1ab152c
penameyo <jhpark3405@gmail.com>
2024-02-04 00:26:23 +0900

    ADD : Java SelfStudy01
    📰 if, if else, switch

--- src/ToSolving/Scan.java
+++ src/ToSolving/Scan.java
@@ -1,4 +1,26 @@
 package ToSolving;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Scan {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		int N = scanner.nextInt();
+
+		for (int i = 1; i <= N; i++) {
+			for (int j= 0; j<N-i; j++) { //공백 + 별의 조합으로 공백이 -로 ?
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
 }
+
+
