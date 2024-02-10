38481a04cdf4e697ca62af4ada114d416b3c8cf2
JongHo Park <jhpark3405@gmail.com>
2024-02-10 20:18:49 +0900

    ADD : Self Study Day 05
    method Study Done

--- src/ToSolving/Scan.java
+++ src/ToSolving/Scan.java
@@ -5,22 +5,18 @@ import java.util.Scanner;
 
 public class Scan {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-		int N = scanner.nextInt();
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
 
-		for (int i = 1; i <= N; i++) {
-			for (int j= 0; j<N-i; j++) { //공백 + 별의 조합으로 공백이 -로 ?
-				System.out.print(" ");
-			}
-			for (int j = 1; j <= i; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-	}
+
+
+        }
+    }
 }
 
 
