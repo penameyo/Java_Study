0004acf5f66b893da28e628e7d99c7dede65a4c3
JongHo Park <jhpark3405@gmail.com>
2024-02-12 16:56:45 +0900

    ADD : Self Study Day 09
    Class / final

---
+++ src/FeedBack/Day09/ex01/YalcoChickenDT.java
@@ -0,0 +1,13 @@
+package FeedBack.Day09.ex01;
+
+public final class YalcoChickenDT extends YalcoChicken {
+    public YalcoChickenDT(int no, String name) {
+        super(no, name);
+    }
+
+    //  ⚠️ 오버라이드 불가
+//    public void fryChicken () {
+//        System.out.println("염지, 반죽입히기, 미원, 튀김");
+    }
+
+    // 생성자 추가할 것
